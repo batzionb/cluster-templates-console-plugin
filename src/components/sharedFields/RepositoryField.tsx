da66be0f185a92f3c9e1117b77e2c60878498c58
Bat Zion Rotman <brotman@redhat.com>
2023-03-16 15:48:26 +0200

    git repo

--- src/components/sharedFields/RepositoryField.tsx
+++ src/components/sharedFields/RepositoryField.tsx
@@ -17,7 +17,10 @@ import CellLoader from '../../helpers/CellLoader';
 import { ArgoCDSecretData, RepositoryType } from '../../types/resourceTypes';
 import { useArgoCDSecrets } from '../../hooks/useArgoCDSecrets';
 import { useAddAlertOnError } from '../../alerts/useAddAlertOnError';
+<<<<<<< HEAD
 import { humanizeUrl } from '../../utils/humanizing';
+=======
+>>>>>>> 65a4f5c (git repo)
 
 type HelmRepositoryFieldProps = {
   fieldName: string;
@@ -127,7 +130,11 @@ const RepositoryField = ({
       {newRepositoryDialogOpen && (
         <NewRepositoryDialog
           closeDialog={() => setNewRepositoryDialogOpen(false)}
+<<<<<<< HEAD
           afterCreate={onNewRepoCreated}
+=======
+          onCreate={onNewRepoCreated}
+>>>>>>> 65a4f5c (git repo)
           predefinedType={type}
         />
       )}
